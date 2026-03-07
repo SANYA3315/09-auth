4cabe535d4fc1acc665ecb1269dc3f06c1b944e8
SANYA3315 <jrchuenko88@gmail.com>
2026-03-07 21:59:40 +0200

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,55 +1,29 @@
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 import { api } from "../../api";
-import { parse } from "cookie";
 import { isAxiosError } from "axios";
 import { logErrorResponse } from "../../_utils/utils";
 
-export async function GET() {
+export async function POST(request: NextRequest) {
   try {
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get("accessToken")?.value;
-    const refreshToken = cookieStore.get("refreshToken")?.value;
+    const body = await request.json();
 
-    if (accessToken) {
-      return NextResponse.json({ success: true });
-    }
+    const res = await api.post("auth/register", body);
 
-    if (refreshToken) {
-      const apiRes = await api.get("auth/session", {
-        headers: {
-          Cookie: cookieStore.toString(),
-        },
-      });
-
-      const setCookie = apiRes.headers["set-cookie"];
-
-      if (setCookie) {
-        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-        for (const cookieStr of cookieArray) {
-          const parsed = parse(cookieStr);
-
-          const options = {
-            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-            path: parsed.Path,
-            maxAge: Number(parsed["Max-Age"]),
-          };
-
-          if (parsed.accessToken)
-            cookieStore.set("accessToken", parsed.accessToken, options);
-          if (parsed.refreshToken)
-            cookieStore.set("refreshToken", parsed.refreshToken, options);
-        }
-        return NextResponse.json({ success: true }, { status: 200 });
-      }
-    }
-    return NextResponse.json({ success: false }, { status: 200 });
+    return NextResponse.json(res.data, { status: 201 });
   } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
-      return NextResponse.json({ success: false }, { status: 200 });
+
+      return NextResponse.json(error.response?.data, {
+        status: error.response?.status || 500,
+      });
     }
+
     logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json({ success: false }, { status: 200 });
+
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
